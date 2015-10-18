38c0156859dcd75183f088bd1666ba61e1b618a7
dariusz75 <darius32@op.pl>
2015-10-18 17:29:13 +0100

    some changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,49 @@
-function myFunction() {
-    
+$(document).ready(function() {
+
+	$("#plus").click(function(event) {
+		// alert('test!');
+		var input = $("#add-item").val(); //collecting value from input window 
+		var item = '<div class="item"> <p>'+ input +' </p></div>';	
+		var done_list = '<div class="done_list"> <p> &#10003; </p> </div>';
+		var delete_list = '<div class="delete_list"> <p> &#10008; </p> </div>';
+		event.preventDefault(); // prevents page reload after submission. 
+		$(".items-list").prepend("<li>"  + item + delete_list + done_list + "</li>");
+		$("#add-item").val(''); //removes text within input box
+
+	});
+
+	$('.items-list').on('click', '.delete_list', function() { 
+		$(this).closest('li').remove(); //removes item
+
+	});
+
+
+	$('.items-list').on('click', '.done_list', function() { //click on check square chance to checked status - white background
+		//alert("test!");
+		$(this).removeClass('done_list').addClass('done_checked');
+		$('.item').removeClass('item').addClass('item_lined');
+
+	}); 
+
+	$('.items-list').on('click', '.done_checked', function() { //click on check square chance to checked status - white background
+		//alert("test!");
+		$(this).removeClass('done_checked').addClass('done_list');
+		$('.item_lined').removeClass('item_lined').addClass('item');
+
+	}); 
+
+});
+
+
+
+/* function myFunction() {
+ 
+$("#plus").click(function(){
+	console.log($("#add-item").val());
+})
+
 }
-$(document).ready(myFunction);
+$(document).ready(myFunction); */
+
+
+//.apend
